81685811e88b85a2eb2d98339ee159a3397f36f3
SzymonAfek <szymon1106@gmail.com>
2020-03-08 11:28:11 +0100

    Wypisanie planszy

--- src/main/java/NewGame.java
+++ src/main/java/NewGame.java
@@ -1,5 +1,15 @@
 public class NewGame {
     public static void main(String[] args) {
-        System.out.println("Hello github");
+
+        char[][] board = {{'x', 'x', 'o'},
+                          {'x', 'o', 'x'},
+                          {' ', ' ', 'o'}};
+
+        for (int lane = 0; lane < board.length; lane++) {
+            for (int column = 0; column < board[lane].length; column++) {
+                System.out.print(board[lane][column] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
